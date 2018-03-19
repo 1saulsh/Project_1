107ec502d00fb5d82099b0d54e0a1e90994099e9
Melissa Gilman <melissadgilman@yahoo.com>
2018-03-19 06:23:03 -0600

    created friendlyCalendar function to fit group format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,19 @@
-//Uses Moment JS to add max and min dates to calendar
-//This just makes the date input fields and drop-down calendar more user friendly; 
-//Not input-validation because the user could still type in a min and max that doesn't meet the criteria... We need an if statement that checks that the date is valid.
-var currentDate = moment().format("YYYY-MM-DD");
-var maxDate = moment(currentDate).add(4, "days").format("YYYY-MM-DD");
+$(document).ready(function () {
+    friendlyCalendar();
 
-// Changing the max and min attributes of the start and end date
-$("#startDate").attr({ "min": currentDate, "max": maxDate, "value": currentDate });
-$("#endDate").attr({ "min": currentDate, "max": maxDate, "value": currentDate });
+    var currentDate = "";
+    var maxDate = "";
 
+    //Uses Moment JS to add max and min dates to calendar & make it more user friendly;
+    function friendlyCalendar() {
+        currentDate = moment().format("YYYY-MM-DD");
+        maxDate = moment(currentDate).add(4, "days").format("YYYY-MM-DD");
+        $("#startDate").attr({ "min": currentDate, "max": maxDate, "value": currentDate });
+        $("#endDate").attr({ "min": currentDate, "max": maxDate, "value": currentDate });
+    };
+
+
+
+
+
+}); //ends the "document.ready" code
