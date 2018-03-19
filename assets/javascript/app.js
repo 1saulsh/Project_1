60b7b58d5d7ffc9224fe413b8d6b35e7acf01753
Melissa Gilman <melissadgilman@yahoo.com>
2018-03-19 06:38:31 -0600

    added Bilal's JS and group's agreed on format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,86 @@
 $(document).ready(function () {
-    friendlyCalendar();
+    // The controller function calls the functions in the desired order
+    controller();
 
-    var currentDate = "";
-    var maxDate = "";
+
+
+    //Variables Declaration
+    var startDate, endDate, currentDate, maxDate, coldWeather, warmWeather, userEmail, city1, city2, city3;
+
+    //Start date of desired stacation
+    startDate = "";
+
+    //End date of desired stacation
+    endDate = "";
+
+    // Current date (today)
+    currentDate = "";
+
+    // The last possible date the user can select
+    maxDate = "";
+
+    //Cold weather activity type
+    coldWeather = "";
+
+    //Warm weather activity type
+    warmWeather = "";
+
+    //User email
+    userEmail = "";
+
+    //Location suggestion 1
+    city1 = "";
+
+    //Location suggestion 2
+    city2 = "";
+
+    //Location suggestion 3
+    city3 = "";
+
+
+    //Functions Declaration
+
+    //Controles program logic
+    function controller() {
+        friendlyCalendar();
+    }
+
+    //User input verification for date format
+    function validateDate(txtDate) {
+        //var regex = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/
+        //return regex.test(date);
+
+        var currVal = txtDate;
+        if (currVal == '')
+            return false;
+
+        var rxDatePattern = "/^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/";
+        var dtArray = currVal.match(rxDatePattern);
+        if (dtArray == null)
+            return false;
+
+        dtMonth = dtArray[1];
+        dtDay = dtArray[3];
+        dtYear = dtArray[5];
+
+        if (dtMonth < 1 || dtMonth > 12)
+            return false;
+        else if (dtDay < 1 || dtDay > 31)
+            return false;
+        else if ((dtMonth == 4 || dtMonth == 6 || dtMonth == 9 || dtMonth == 11) && dtDay == 31)
+            return false;
+        else if (dtMonth == 2) {
+            var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+            if (dtDay > 29 || (dtDay == 29 && !isleap))
+                return false;
+        }
+        return true;
+    }
+
+    //Stores user input dates
+    function storeDates() {
+
+    }
 
     //Uses Moment JS to add max and min dates to calendar & make it more user friendly;
     function friendlyCalendar() {
@@ -13,7 +91,47 @@ $(document).ready(function () {
     };
 
 
+    //Populates activity options based on user input
+    function filterOptions() {
 
+    }
 
+    //Bring back weather API
+    function weatherRequest() {
+
+    }
+
+    //Bring back map API
+    function mapRequest() {
+
+    }
+
+    //Creates location suggestions
+    function suggestLocations() {
+
+    }
+
+    //Populates location suggestions
+    function outputLocations() {
+
+    }
+
+    //Expands location suggestions
+    function expandSuggestion() {
+
+    }
+
+    //User input verification for email format
+    function validateEmail() {
+        function isEmail(email) {
+            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return regex.test(email);
+        }
+    }
+
+    //Stores user email
+    function storeEmail() {
+
+    }
 
 }); //ends the "document.ready" code
