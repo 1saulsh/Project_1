ea3ff976b5e3ef9b2bbe1b0b3429e7dd853c0597
Melissa Gilman <melissadgilman@yahoo.com>
2018-03-19 05:52:09 -0600

    Fixed invalid min and max dates for calendar.

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 //Not input-validation because the user could still type in a min and max that doesn't meet the criteria... We need an if statement that checks that the date is valid.
 var currentDate = moment().format("YYYY-MM-DD");
 var maxDate = moment(currentDate).add(4, "days").format("YYYY-MM-DD");
-console.log("MAX DATE: " + maxDate);
 
 // Changing the max and min attributes of the start and end date
-$("#startDate").attr({ "min": currentDate, "max": maxDate });
-$("#endDate").attr({ "min": currentDate, "max": maxDate });
+$("#startDate").attr({ "min": currentDate, "max": maxDate, "value": currentDate });
+$("#endDate").attr({ "min": currentDate, "max": maxDate, "value": currentDate });
